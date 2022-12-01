3a374666fe3b88e69fce74cb0f0bee9557c5b93d
S5640603 <115167282+S5640603@users.noreply.github.com>
2022-12-01 11:52:13 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,14 +1,16 @@
 #include "lib.h"
 bool Nprimo(int n){
 
-    if (n < 1)
+    if (n < 1){
         return false;
-    else if (n == 2, n== 1)
+    }else if (n == 2, n== 1){
         return true;
+    }
 
     for (int i = 2; i < n; i++)
-        if (n % i == 0)
+        if (n % i == 0){
             return false;
-  
+        }
+    
     return true;
 }

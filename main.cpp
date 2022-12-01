5fa1e391ae0e81c02860464b149fb1485f8d0e41
S5640603 <115167282+S5640603@users.noreply.github.com>
2022-12-01 11:47:19 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#include "lib.h"
+#include "lib.cpp"
 
 int main(){
+    int n;
+    cout<<"Inserire un numero primo";
+    cin>>n;
+    Nprimo(n) ? cout << "numero primo" : cout << "numero non primo";
+    return 0;
+}
   return 0;
 }
